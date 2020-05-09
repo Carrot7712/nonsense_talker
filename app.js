3bcc83a941be05d2b952612b1deee43e640ecc5c
Carrot7712 <carrot7712@gmail.com>
2020-05-09 16:48:04 +0800

    save radio checked

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 //include packages and define server related variables
-const express=require('express')
-const exphbs=require('express-handlebars')
-const bodyParser=require('body-parser')
-const generateNonsense =require('./generate_nonsense')
-const app=express()
-const port=3000
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateNonsense = require('./generate_nonsense')
+const app = express()
+const port = 3000
 
 //setting template engine
-app.engine('handlebars',exphbs({defaultLayout:'main'}))
-app.set('view engine','handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 //use static files
 app.use(express.static('public'))
-//setting body-parser 
-app.use(bodyParser.urlencoded({extended:true}))
+//setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
 //starts the express server and listening for connections
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Express app is listening on port ${port}`)
 })
 
 //setting routes
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.render('index')
 })
-app.post('/',(req,res)=>{
+app.post('/', (req, res) => {
   const sentence = generateNonsense(req.body)
-  res.render('index',{sentence})
+  //想保存checked的狀態
+  const options = req.body
+  const engineer = function(){
+    if(options.profession ==='engineer')
+    return options.profession
+  }
+  const designer = function () {
+    if (options.profession === 'designer') return options.profession
+  }
+  const entrepreneur = function () {
+    if (options.profession === 'entrepreneur') return options.profession
+  }
+  res.render('index', { sentence, options, engineer, designer, entrepreneur })
 })
